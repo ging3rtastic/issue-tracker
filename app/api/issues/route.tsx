27c25b4a2c38bb08f922d4715a783803203c5d57
Tristyn Spies <tristyn.spies@cullinansystems.co.za>
2025-08-17 16:50:19 +0200

    Typo Fix

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -16,6 +16,7 @@ export async function POST(request: NextRequest) {
     if (!data.success) {
         return new Response(JSON.stringify({ error: data.error.issues }), {
             status  : 400,
+
         });
     }
     
