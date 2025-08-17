a0cdaa4a762a484071d2f217246ad3ddfceadccb
Tristyn Spies <tristyn.spies@cullinansystems.co.za>
2025-08-17 19:06:18 +0200

    simple client side validation for issue

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -2,11 +2,8 @@ import { NextRequest, NextResponse }    from 'next/server';
 import { z }                            from 'zod';
 
 import prisma                           from '@/lib/prisma';
+import { createIssueSchema }            from '@/app/validationSchemas';
 
-const createIssueSchema = z.object({
-    title       : z.string().min(1, "Title is required"),
-    description : z.string().min(1, "Description is required"),
-});
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,27 +2,27 @@
 
 import "easymde/dist/easymde.min.css";
 
-import { Button,Callout,TextArea,TextField }    from '@radix-ui/themes'
-import { MdOutlineSubtitles, MdErrorOutline }   from "react-icons/md";
-import { useForm, Controller }                  from 'react-hook-form';
-import { useRouter }                            from 'next/navigation';
+import { Button,Callout,TextArea,TextField,Text }   from '@radix-ui/themes'
+import { MdOutlineSubtitles, MdErrorOutline }       from "react-icons/md";
+import { useForm, Controller }                      from 'react-hook-form';
+import { createIssueSchema }                        from "@/app/validationSchemas";
+import { useRouter }                                from 'next/navigation';
+import { zodResolver }                              from '@hookform/resolvers/zod';
+import { z }                                        from 'zod';
 
-import axios                                    from 'axios';
-import Link                                     from 'next/link';
-import React, { useState }                      from 'react'
+import axios                                        from 'axios';
+import Link                                         from 'next/link';
+import React, { useState }                          from 'react'
 
 // import SimpleMDE                            from "react-simplemde-editor";
 
-interface CreateIssueForm {
-    title       : string;
-    description : string;
-}
+type CreateIssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
 
     const router = useRouter();
 
-    const { register, control, handleSubmit, formState: { errors } } = useForm<CreateIssueForm>();
+    const { register, control, handleSubmit, formState: { errors } } = useForm<CreateIssueForm>({resolver: zodResolver(createIssueSchema)});
 
     const [error, setError] = useState('');
 
@@ -53,13 +53,15 @@ const NewIssuePage = () => {
             )}
         >
            
-            <TextField.Root placeholder="Title for your issue..." {...register("title", { required: "Title is required" })}>
+            <TextField.Root placeholder="Title for your issue..." {...register("title")}>
                 <TextField.Slot>
                     <MdOutlineSubtitles />
                 </TextField.Slot>
             </TextField.Root>
+            {errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
 
             <TextArea placeholder='Description' {...register("description")}/>
+            {errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
 
             <Button type="submit">
                 Create Issue
